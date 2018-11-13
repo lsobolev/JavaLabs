f0c10e1267ccf3b4c4018c22a31eb02d8c3659d6
L <ap.qwert@yandex.ru>
2018-11-13 21:31:53 +0300

    fixed class Letter

--- lab7/src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task5/model/Letter.java
+++ lab7/src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task5/model/Letter.java
@@ -2,22 +2,27 @@ package by.bntu.fitr.povt.justcompileit.javalabs.lab07.task5.model;
 
 public class Letter {
 
-    public static boolean isVowels(char letter) {
-        switch (Character.toLowerCase(letter)) {
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-            case 'y':
-                return true;
-            default:
-                return false;
-        }
-    }
+    public static String isVowel(char letter) {
 
-    public static boolean isConsonant(char letter) {
-        return !isVowels(letter);
+        StringBuilder result = new StringBuilder();
+
+        if (Character.isLetter(letter)) {
+            switch (Character.toLowerCase(letter)) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'y':
+                    result.append("is vowel");
+                    break;
+                default:
+                    result.append("is consonant");
+            }
+        } else {
+            result.append("not a letter");
+        }
+        return result.toString();
     }
 
 }
