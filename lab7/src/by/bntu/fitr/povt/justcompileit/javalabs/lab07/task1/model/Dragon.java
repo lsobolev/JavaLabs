6f54284fe0f4c168705c690e6a5cb298c70b4379
L <ap.qwert@yandex.ru>
2018-11-13 21:06:33 +0300

    fix Dragon

--- lab7/src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task1/model/Dragon.java
+++ lab7/src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task1/model/Dragon.java
@@ -3,44 +3,67 @@ package by.bntu.fitr.povt.justcompileit.javalabs.lab07.task1.model;
 
 public class Dragon {
 
+    public final int FIRST_PERIOD = 200;
+    public final int COUNT_HEAD_FIRST_PERIOD = 3;
+    public final int STEP_NEXT_PERIOD = 100;
+    public final int COUNT_HEAD_SECOND_PERIOD = 2;
+
     private String name;
     private int numHead;
     private int numEyes;
     private int age;
 
     public Dragon(String name, int age) {
-        numHead = 3;
-        numEyes = numHead * 2;
         this.name = name;
-        this.age = age;
+        this.numHead = 3;
+        this.numEyes = numHead * 2;
+        this.age = this.ageValidation(age);
         this.calculateHead();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumEyes() {
+        return numEyes;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     public int getNumHead() {
         return numHead;
     }
 
+    private int ageValidation(int age) {
+        if (age >= 0) {
+            return age;
+        } else {
+            return 0;
+        }
+    }
+
     private void calculateHead() {
 
         int age = this.age;
 
-        if (age >= 200) {
-            numHead += 200 * 3;
-            age -= 200;
+        if (age >= FIRST_PERIOD) {
+            numHead += FIRST_PERIOD * COUNT_HEAD_FIRST_PERIOD;
+            age -= FIRST_PERIOD;
         } else {
-            numHead += age * 3;
-            age = 0;
+            numHead += age * COUNT_HEAD_FIRST_PERIOD;
         }
 
-        if (age >= 100) {
-            numHead += 100 * 2;
-            age -= 100;
+        if (age >= STEP_NEXT_PERIOD) {
+            numHead += STEP_NEXT_PERIOD * COUNT_HEAD_SECOND_PERIOD;
+            age -= STEP_NEXT_PERIOD;
         } else {
-            numHead += age * 2;
-            age = 0;
+            numHead += age * COUNT_HEAD_SECOND_PERIOD;
         }
+
         numHead += age;
-
         numEyes = numHead * 2;
     }
 
@@ -48,8 +71,8 @@ public class Dragon {
     @Override
     public String toString() {
         return "\nDragon: " + name +
+                "\nAge: " + age +
                 "\nNum Head: " + numHead +
-                "\nNum Eyes: " + numEyes +
-                "\nAge: " + age;
+                "\nNum Eyes: " + numEyes;
     }
 }
