2d9d22dd462a0dcff225d3bc3697267f33224383
L <ap.qwert@yandex.ru>
2018-11-14 10:58:55 +0300

    added overload for method

--- lab7/src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task2/model/Number.java
+++ lab7/src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task2/model/Number.java
@@ -2,30 +2,33 @@ package by.bntu.fitr.povt.justcompileit.javalabs.lab07.task2.model;
 
 public class Number {
 
-    public static String maxNumber(double num1, double num2, double num3) {
+    public static int maxNumber(int num1, int num2, int num3) {
 
-        StringBuilder result = new StringBuilder();
-
-        if (num1 == num2 && num1 == num3) {
-            result.append("\nAll numbers are equal!\n");
-        } else if (num1 == num2) {
-            result.append("\nFirst number is the equal to the second number!\n");
-        } else if (num1 == num3) {
-            result.append("\nFirst number is the equal to the third number!\n");
-        } else if (num2 == num3) {
-            result.append("\nSecond number is the equal to the third!\n");
-        }
+        int result;
 
         if (num1 > num2 && num1 > num3) {
-            result.append("Max num: ");
-            result.append(Double.toString(num1));
-        } else if (num2 > num3 && num2 > num3) {
-            result.append("Max num: ");
-            result.append(Double.toString(num2));
+            result = num1;
+        } else if (num2 > num1 && num2 > num3) {
+            result = num2;
         } else {
-            result.append("Max num: ");
-            result.append(Double.toString(num3));
+            result = num3;
         }
-        return result.toString();
+
+        return result;
+    }
+
+    public static double maxNumber(double num1, double num2, double num3) {
+
+        double result;
+
+        if (num1 > num2 && num1 > num3) {
+            result = num1;
+        } else if (num2 > num1 && num2 > num3) {
+            result = num2;
+        } else {
+            result = num3;
+        }
+
+        return result;
     }
 }
